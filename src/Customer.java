8fbe37c11ceb4e426ccaf4913ba5ba531d4b33fd
KKochen <e.c.kochen@hotmail.com>
2019-07-01 11:53:38 +0200

    Setup of the Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,48 @@
 
 public class Customer {
-
+	private int customerId;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String emailAddress;
+	
+	public int getCustomerId() {
+		return customerId;
+	}
+	
+	public void setCustomerId(int id) {
+		this.customerId = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String name) {
+		this.firstName = name;
+	}
+	
+	public String getMiddleName() {
+		return middleName;
+	}
+	
+	public void setMiddleName(String name) {
+		this.middleName = name;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String name) {
+		this.lastName = name;
+	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	
+	public void setEmailAddress(String email) {
+		this.emailAddress = email;
+	}
 }
