99f9364868d8933e08235c17d312dc21e3599c52
KKochen <e.c.kochen@hotmail.com>
2019-07-01 11:47:49 +0200

    setup of the Article class

--- src/Article.java
+++ src/Article.java
@@ -1,4 +1,39 @@
 
 public class Article {
-
+	private int articleId;
+	private String articleName;
+	private double articlePrice;
+	private int articleAmount;
+	
+	public int getArticleId() {
+		return articleId;
+	}
+	
+	public void setArticleId(int id) {
+		this.articleId = id;
+	}
+	
+	public String getArticleName() {
+		return articleName;
+	}
+	
+	public void setArticleName(String name) {
+		this.articleName = name;
+	}
+	
+	public double getArticlePrice() {
+		return articlePrice;
+	}
+	
+	public void setArticlePrice(double price) {
+		this.articlePrice = price;
+	}
+	
+	public int getArticleAmount() {
+		return articleAmount;
+	}
+	
+	public void setArticleAmount(int amount) {
+		this.articleAmount = amount;
+	}
 }
